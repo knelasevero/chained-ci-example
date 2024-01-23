7d750d3c66502f74a5e8bd65dad6bf9f733d95de
Lucas Severo Alves <lseveroa@redhat.com>
2024-01-23 20:03:14 +0100

    secure server with timeouts

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -11,9 +12,17 @@ func main() {
 		fmt.Fprintln(w, "Hello from a Platform Engineer!")
 	})
 
-	// Set the server to listen on port 8080
+	// Create a new server and specify the timeouts
+	server := &http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  5 * time.Second,   // Timeout for reading the request
+		WriteTimeout: 10 * time.Second,  // Timeout for writing the response
+		IdleTimeout:  15 * time.Second,  // Timeout for idle connections
+	}
+
+	// Start the server
 	fmt.Println("Server starting on port 8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := server.ListenAndServe(); err != nil {
 		fmt.Println("Error starting server:", err)
 	}
 }
